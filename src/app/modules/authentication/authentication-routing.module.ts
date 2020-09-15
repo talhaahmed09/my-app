09e5dbba8384e561c84c1af77f45ad3da1d20da5
talhaDotAhmed <talha@blockshift.com.pk>
2020-09-15 17:11:27 +0500

    fff

--- src/app/modules/authentication/authentication-routing.module.ts
+++ src/app/modules/authentication/authentication-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from 'src/app/login/login.component';
+import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
